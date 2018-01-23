17bf85605ca46dc1869c9838a78ccf0f26042f9d
Joabe Costa <joabetc@hotmail.com>
2018-01-23 16:05:59 -0200

    Fixing Optimize CSS setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV == 'production') {
     plugins.push(new optimizeCSSAssetsPlugin({
         cssProcessor: require('cssnano'),
         cssProcessorOptions: {
-            discartComments: {
+            discardComments: {
                 removeAll: true
             }
         },
